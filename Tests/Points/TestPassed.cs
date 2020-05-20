7dea8188d0355e0fddcd73f7b35dc4605c051f81
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-05-20 14:04:56 +0300

    Added new unit tests for MethodTestResult

--- Tests/Points/TestPassed.cs
+++ Tests/Points/TestPassed.cs
@@ -13,24 +13,55 @@ namespace TestMyCode.CSharp.Tests.Passed
     {
         private Assembly Assembly = Assembly.Load("TestAssembly");
         private TestProjectData TestProjectData = new TestProjectData();
+        private ProjectTestRunner testRunner;
 
         public TestPassed()
         {
             this.TestProjectData.LoadAssembly(this.Assembly);
+
+            testRunner = new ProjectTestRunner(TestProjectData);
+            testRunner.RunAssemblyTests(Assembly);
         }
 
         [Theory]
         [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysPasses), true)]
         [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysFails), false)]
-        public void ClassHasCorrectPassed(Type methodClass, string methodName, bool expect)
+        public void MethodResultsCorrectPassed(Type methodClass, string methodName, bool expect)
         {
-            ProjectTestRunner testRunner = new ProjectTestRunner(TestProjectData);
-
-            testRunner.RunAssemblyTests(Assembly);
-
             bool passed = testRunner.TestResults.FirstOrDefault(result => result.Name == $"{methodClass.FullName}.{methodName}").Passed;
 
             Assert.Equal(passed, expect);
+        }
+
+        [Theory]
+        [InlineData(typeof(TestAssembly.TestPassed), "TestThatAlwaysPasses", true)]
+        [InlineData(typeof(TestAssembly.TestPassed), "TestThatAlwaysFails", true)]
+        [InlineData(typeof(TestAssembly.TestPassed), "TestThatDoesNotExist", false)]
+        public void MethodWithGivenNameCanBeFound(Type methodClass, string methodName, bool expect)
+        {
+            MethodTestResult found = testRunner.TestResults.FirstOrDefault(result => result.Name == $"{methodClass.FullName}.{methodName}");
+
+            Assert.Equal(found != null, expect);
+        }
+
+        [Theory]
+        [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysFails), true)]
+        [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysPasses), false)]
+        public void ErrorStackTraceCanBeFoundAfterFailing(Type methodClass, string methodName, bool expect)
+        {
+            IList<string> stackTrace = testRunner.TestResults.FirstOrDefault(result => result.Name == $"{methodClass.FullName}.{methodName}").ErrorStackTrace;
+
+            Assert.Equal(stackTrace.Count > 0, expect);
+        }
+
+        [Theory]
+        [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysFails), true)]
+        [InlineData(typeof(TestAssembly.TestPassed), nameof(TestAssembly.TestPassed.TestThatAlwaysPasses), false)]
+        public void MessageCanBeFoundAfterFailing(Type methodClass, string methodName, bool expect)
+        {
+            string message = testRunner.TestResults.FirstOrDefault(result => result.Name == $"{methodClass.FullName}.{methodName}").Message;
+
+            Assert.Equal(message != "", expect);
         }
     }
 }
